3be004d1ebe7ae3b2cbc2ef0d763f12c4df06a11
Ryota Tanaka <63334579+rtanaka-lab@users.noreply.github.com>
2024-04-03 10:41:25 +0900

    Update ai2d.sh

--- download_scripts/ai2d.sh
+++ download_scripts/ai2d.sh
@@ -4,4 +4,5 @@ echo "Donwloading AI2D dataset..."
 mkdir ai2d
 cd ai2d
 wget https://ai2-public-datasets.s3.amazonaws.com/diagrams/ai2d-all.zip
+wget https://s3-us-east-2.amazonaws.com/prior-datasets/ai2d_test_ids.csv
 unzip ai2d-all.zip && rm ai2d-all.zip
